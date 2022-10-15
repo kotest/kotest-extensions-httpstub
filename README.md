20342d9199f84b51f29846c43095630a10cf675c
sksamuel <sam@sksamuel.com>
2022-10-15 06:11:13 -0500

    Project bootstrap

--- README.md
+++ README.md
@@ -1,2 +1,12 @@
 # kotest-extensions-httpstub
+
 Extensions for kotest to stub external http services
+
+Please create issues on the main kotest [board](https://github.com/kotest/kotest/issues).
+
+[![Build Status](https://github.com/kotest/kotest-extensions-wiremock/workflows/master/badge.svg)](https://github.com/kotest/kotest-extensions-wiremock/actions)
+[<img src="https://img.shields.io/maven-central/v/io.kotest.extensions/kotest-extensions-wiremock.svg?label=latest%20release"/>](http://search.maven.org/#search|ga|1|kotest-extensions-wiremock)
+![GitHub](https://img.shields.io/github/license/kotest/kotest-extensions-wiremock)
+[![kotest @ kotlinlang.slack.com](https://img.shields.io/static/v1?label=kotlinlang&message=kotest&color=blue&logo=slack)](https://kotlinlang.slack.com/archives/CT0G9SD7Z)
+[<img src="https://img.shields.io/nexus/s/https/oss.sonatype.org/io.kotest.extensions/kotest-extensions-wiremock.svg?label=latest%20snapshot"/>](https://oss.sonatype.org/content/repositories/snapshots/io/kotest/extensions/kotest-extensions-wiremock/)
+
---
+++ build.gradle.kts
@@ -0,0 +1,107 @@
+import org.gradle.api.tasks.testing.logging.TestExceptionFormat
+import org.jetbrains.kotlin.gradle.tasks.KotlinCompile
+
+plugins {
+   `java-library`
+   signing
+   `maven-publish`
+   kotlin("jvm") version "1.6.21"
+}
+
+group = "io.kotest.extensions"
+version = Ci.version
+
+repositories {
+   mavenCentral()
+   maven {
+      url = uri("https://oss.sonatype.org/content/repositories/snapshots")
+   }
+}
+
+dependencies {
+   api(libs.wiremock)
+   implementation(libs.kotest.api)
+   testImplementation(libs.kotest.runner)
+   testImplementation(libs.kotest.assertions)
+}
+
+tasks.test {
+   useJUnitPlatform()
+   testLogging {
+      showExceptions = true
+      showStandardStreams = true
+      exceptionFormat = TestExceptionFormat.FULL
+   }
+}
+
+tasks.withType<KotlinCompile> {
+   kotlinOptions.jvmTarget = "1.8"
+}
+
+val signingKey: String? by project
+val signingPassword: String? by project
+
+val publications: PublicationContainer = (extensions.getByName("publishing") as PublishingExtension).publications
+
+signing {
+   useGpgCmd()
+   if (signingKey != null && signingPassword != null) {
+      @Suppress("UnstableApiUsage")
+      useInMemoryPgpKeys(signingKey, signingPassword)
+   }
+   if (Ci.isRelease) {
+      sign(publications)
+   }
+}
+
+java {
+   withJavadocJar()
+   withSourcesJar()
+}
+
+publishing {
+   repositories {
+      maven {
+         val releasesRepoUrl = uri("https://oss.sonatype.org/service/local/staging/deploy/maven2/")
+         val snapshotsRepoUrl = uri("https://oss.sonatype.org/content/repositories/snapshots/")
+         name = "deploy"
+         url = if (Ci.isRelease) releasesRepoUrl else snapshotsRepoUrl
+         credentials {
+            username = System.getenv("OSSRH_USERNAME") ?: ""
+            password = System.getenv("OSSRH_PASSWORD") ?: ""
+         }
+      }
+   }
+
+   publications {
+      register("mavenJava", MavenPublication::class) {
+         from(components["java"])
+         pom {
+            name.set("kotest-extensions-wiremock")
+            description.set("Kotest extension for wiremock")
+            url.set("https://www.github.com/kotest/kotest-extensions-wiremock")
+
+            scm {
+               connection.set("scm:git:http://www.github.com/kotest/kotest-extensions-wiremock")
+               developerConnection.set("scm:git:http://github.com/sksamuel")
+               url.set("https://www.github.com/kotest/kotest-extensions-wiremock")
+            }
+
+            licenses {
+               license {
+                  name.set("The Apache 2.0 License")
+                  url.set("https://opensource.org/licenses/Apache-2.0")
+               }
+            }
+
+            developers {
+               developer {
+                  id.set("sksamuel")
+                  name.set("Stephen Samuel")
+                  email.set("sam@sksamuel.com")
+               }
+            }
+         }
+      }
+   }
+}
