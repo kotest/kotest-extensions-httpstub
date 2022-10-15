8d56a9df82c66d81bf3d753cea239441187a4886
sksamuel <sam@sksamuel.com>
2022-10-15 06:24:59 -0500

    Updated publish repos

---
+++ src/main/kotlin/io/kotest/extensions/httpstub/dsl.kt
@@ -0,0 +1,11 @@
+package io.kotest.extensions.httpstub
+
+fun http(configure: Http.() -> Unit) {}
+
+class Http {
+
+}
+
+fun post() {
+
+}
---
+++ src/test/kotlin/io/kotest/extensions/httpstub/DslTest.kt
@@ -0,0 +1,9 @@
+package io.kotest.extensions.httpstub
+
+import io.kotest.core.spec.style.FunSpec
+
+class DslTest : FunSpec() {
+   init {
+
+   }
+}
